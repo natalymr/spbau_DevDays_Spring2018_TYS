f104ff1d9b1fe158156e261f17b6ad01faca9709
lergor <gor.ler177@gmail.com>
2018-05-03 14:52:18 +0300

    Adde app file.

---
+++ src/application.py
@@ -0,0 +1,13 @@
+import sys
+from src.window import Window
+from PyQt5.QtWidgets import QApplication
+
+class App:
+
+    def __init__(self):
+        self.__app = QApplication(sys.argv)
+
+    def run(self):
+        w = Window()
+        w.show()
+        sys.exit(self.__app.exec_())
