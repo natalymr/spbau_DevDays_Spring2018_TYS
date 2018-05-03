c80396d161c45a9640325362791b1b7ac3c16e9a
Дмитрий Гостевский <selp@10-181-193-120.client.airnet.ru>
2018-05-04 00:46:34 +0300

    task_code

--- src/code_window.py
+++ src/code_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QFrame, QSplitter
+from PyQt5.QtWidgets import QFrame, QSplitter, QTextEdit, QPushButton, QWidget
 from PyQt5.QtCore import Qt
 
 
@@ -7,10 +7,28 @@ class WindowCode(QSplitter):
     def __init__(self, window):
         self.window = window
         super().__init__(Qt.Vertical)
-        fst_frame = QFrame(self)
-        fst_frame.setFrameShape(QFrame.StyledPanel)
+
+        fst_frame = QTextEdit()
+        fst_frame.setFontPointSize(8)
+        fst_frame.setTabStopWidth(24)
+
         snd_frame = QFrame(self)
         snd_frame.setFrameShape(QFrame.StyledPanel)
         self.addWidget(fst_frame)
         self.addWidget(snd_frame)
-        self.setSizes([self.width(), 0])
+        self.setSizes([self.width(),
+                       0])
+
+        btn = QPushButton('Check', self)
+        btn.move(0, 0)
+        btn.clicked.connect(self.check)
+
+    def check(self):
+        self.SW = SecondWindow()
+        self.SW.show()
+
+
+class SecondWindow(QWidget):
+    def __init__(self):
+        super(SecondWindow, self).__init__()
+        self.setWindowTitle('Result')
--- src/task_window.py
+++ src/task_window.py
@@ -1,22 +1,58 @@
-from PyQt5.QtWidgets import QFrame, QSplitter
+from PyQt5.QtWidgets import QFrame, QSplitter, QProgressBar
 from PyQt5.QtCore import Qt
+from PyQt5 import QtCore, QtGui
 from src.application import *
-
 
 class WindowTask(QSplitter):
 
     def __init__(self, window):
         self.window = window
         super().__init__(Qt.Vertical)
+
+
+
         fst_frame = QFrame(self)
         fst_frame.setFrameShape(QFrame.StyledPanel)
-        snd_frame = QFrame(self)
-        snd_frame.setFrameShape(QFrame.StyledPanel)
+
+
+        self.snd_frame = QProgressBar(self)
+        self.snd_frame.setValue(0)
+        palette = QtGui.QPalette(self.palette())
+        palette.setColor(QtGui.QPalette.Highlight,
+                         QtGui.QColor(QtCore.Qt.green))
+        self.snd_frame.setPalette(palette)
+        self.val = 0.00
+        self.snd_frame.setFormat('0.00')
+        self.totalMin = 1
+
+
         self.addWidget(fst_frame)
-        self.addWidget(snd_frame)
-        self.setSizes([self.width(),
-                       0])
+        self.addWidget(self.snd_frame)
+        self.setSizes([self.width() * 11 // 12,
+                       self.width() // 12])
+        
+
+        self.count = 1
+        self.scale = 1000
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.tick_status)
+        self.timer.start(1000)
 
     def update_task(self, task):
         if task.type == TaskType.CODING:
-            pass
+            pass  
+
+    def tick_status(self):
+        min = int(self.count/60)
+        sec = int(self.count % 60)
+        self.val = min + sec/100
+        self.snd_frame.setValue(self.count*100/(self.totalMin*60))
+        self.snd_frame.setFormat('%.02f' % self.val)
+        self.count += 1
+
+
+
+
+
+
+
