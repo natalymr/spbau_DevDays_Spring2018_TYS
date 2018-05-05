02537710190521b7c4e4bffd1746f73b9fb761e5
GostD <dimaga92@gmail.com>
2018-05-05 12:07:36 +0300

    tex parse/ language chose

--- src/code_window.py
+++ src/code_window.py
@@ -1,4 +1,6 @@
 import os
+
+from PyQt5.QtWidgets import QComboBox
 from PyQt5.QtWidgets import QFrame, QSplitter, QTextEdit, QPushButton, QWidget
 from PyQt5.QtCore import Qt
 from PyQt5 import QtGui
@@ -21,51 +23,90 @@ class WindowCode(QSplitter):
         self.fst_frame.setTabStopWidth(36)
         self.fst_frame.clear()
 
-        snd_frame = QFrame(self)
-        snd_frame.setFrameShape(QFrame.StyledPanel)
-        self.addWidget(self.fst_frame)
-        self.addWidget(snd_frame)
-        self.setSizes([self.width(),
-                       0])
+        # snd_frame = QFrame(self)
+        # snd_frame.setFrameShape(QFrame.StyledPanel)
+        self.snd_frame = QSplitter(Qt.Horizontal)
+        self.left = QSplitter(Qt.Horizontal)
+        self.right = QSplitter(Qt.Horizontal)
+        self.snd_frame.addWidget(self.left)
+        self.snd_frame.addWidget(self.right)
+        self.snd_frame.setSizes([8, 1])
+        self.snd_frame.setHandleWidth(0)
 
-        btn = QPushButton('Check', self)
+
+        #preprocessing, cmd, filename
+        self.compilers = {"C++11": ['g++ test.cpp -std=c++11', './a.out', 'test.cpp'], "Python": ['', 'python3 test.py', 'test.py'],
+                          "Java8": ['', '', ''], "Haskell": ['', '', '']}
+
+
+        self.combo = QComboBox(self.right)
+        self.languages = ["C++11", "Python"]#,
+                        #"Java8", "Haskell"]
+        self.combo.addItems(self.languages)
+
+        self.language = "C++11"
+        self.preprocessing = self.compilers[self.language][0]
+        self.cmd_pref = 'cat input.txt  | ' + self.compilers[self.language][1]
+        # pref = '(cat input.txt  | ' + str(compiler) + ''
+        self.cmd_suff = ' 1>temp.txt 2>err.txt'
+        self.file =self.compilers[self.language][2]
+
+        self.combo.activated[str].connect(self.onActivated)
+
+        self.addWidget(self.fst_frame)
+        self.addWidget(self.snd_frame)
+        self.setSizes([self.width()*19/20,
+                       self.width()/20])
+
+        btn = QPushButton('Check', self.left)
         btn.move(0, 0)
         btn.clicked.connect(self.check)
 
+
+
     def check(self):
         textInWindow = self.fst_frame.toPlainText()
-        self.SW = SecondWindow(textInWindow)
+        self.SW = SecondWindow(textInWindow, self)
         self.SW.show()
+
+    def onActivated(self, text):
+        self.language = text
+        self.preprocessing = self.compilers[self.language][0]
+        self.cmd_pref = 'cat input.txt  | ' + self.compilers[self.language][1]
+        self.cmd_suff = ' 1>temp.txt 2>err.txt'
+        self.file = self.compilers[self.language][2]
 
 
 class SecondWindow(QWidget):
-    def __init__(self, textIn):
+    def __init__(self, textIn, mainWindow):
         super(SecondWindow, self).__init__()
         self.setWindowTitle('Result')
         self.resize(200, 200)
-        file = 'test.cpp'#py
+        self.mainWindow = mainWindow
+
+        file = self.mainWindow.file#'test.cpp'#py
         f = open(file, 'w')
         f.write(textIn)
         f.close()
 
         # compiler = 'python3'
-
-        p = subprocess.Popen('g++ test.cpp', shell=True)
-        p.wait()
-
-        compiler = './a.out'
-
+        if not self.mainWindow.preprocessing == '':
+            p = subprocess.Popen(self.mainWindow.preprocessing, shell=True)
+            p.wait()
+        # compiler = './a.out'
         input = "4 4\n0 1 2 5\n1 0 3 4\n2 3 0 7\n5 4 7 0\n"
         # input = "input.txt"
         fo = open("input.txt", 'w')
         fo.write(input)
         fo.close()
-        pref = '(cat ' + "input.txt" + ' | ' + str(compiler) + ''
-        suff = ')>temp.txt'
+        # pref = '(cat ' + "input.txt" + ' | ' + str(compiler) + ''
+        # suff = ')>temp.txt'
         expectedRes = "YES\n2 1 1\n1 3 2\n2 4 4\n1 4 5\n"
+        if self.mainWindow.language == "Python":
+            expectedRes += '\n'
         # expectedRes += '\n'
         # p = subprocess.Popen(pref+file+suff, shell=True)
-        p1 = subprocess.Popen(pref + suff, shell=True)
+        p1 = subprocess.Popen(self.mainWindow.cmd_pref + self.mainWindow.cmd_suff, shell=True)
         p1.wait()
         resFile = open("temp.txt", "r")
         resLst = resFile.readlines()
@@ -76,12 +117,16 @@ class SecondWindow(QWidget):
         result = ''
         os.remove(file)
         os.remove("temp.txt")
+        os.remove("err.txt")
         os.remove("input.txt")
-        os.remove("a.out")
+        if self.mainWindow.language == 'C++11' and not res == '':
+            os.remove("a.out")
         if res == expectedRes:
             result += 'OK'
+        elif res == '':
+            result += 'Error'
         else:
-            result += 'False'
+            result += 'Wrong'
         self.text = QLabel(self)
         self.text.setText(result)
         self.text.move(80, 80)
--- src/task_window.py
+++ src/task_window.py
@@ -1,12 +1,27 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QTextDocument
+from PyQt5.QtWidgets import QFrame, QSplitter, QProgressBar, QToolBox, QLabel, QTableWidgetItem
 from PyQt5.QtCore import Qt
 from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QLabel
+import subprocess
+import threading
+import time
+import sys
+import matplotlib as mpl
+from PyQt5.QtWidgets import QTableWidget
+from PyQt5.QtWidgets import QTextEdit
+from PyQt5.QtWidgets import QWidget
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import json
+
+from src.application import *
 
 class WindowTask(QSplitter):
 
     def __init__(self, window):
         self.window = window
-        super(WindowTask, self).__init__(Qt.Vertical)
+        super().__init__(Qt.Vertical)
 
         self.setHandleWidth(0)
 
@@ -14,92 +29,60 @@ class WindowTask(QSplitter):
         self.fst_frame.setHandleWidth(0)
 
         self.up_frame = QFrame(self)
-        # self.up_frame.setFrameShape(QFrame.StyledPanel)
         self.up_text = QLabel(self.up_frame)
 
 
-        # f = open("coding_problems.json", "r")
-        #
-        # st = f.readline()
-        # st = f.readline()
-        # st = f.readline()
-        # st = f.readline()
-        # st = st.replace("'", "\"")
-        # ste = json.loads(st)
-        # name = ste["problems_name"]
-        #
-        # examples_input = ste["sampleTests"]
-        #
-        # rows = len(examples_input)
-        #
-        # self.tableWidget = QTableWidget()
-        # self.tableWidget.setRowCount(rows)
-        # self.tableWidget.setColumnCount(2)
+        f = open("coding_problems.json", "r")
 
-        # dt = ["input", "output"]
-        # for i in range(rows):
-        #     for j in range(2):
-        #         self.tableWidget.setItem(i, j, QTableWidgetItem(examples_input[i][dt[j]]))
+        st = f.readline()
+        st = f.readline()
+        st = f.readline()
+        st = f.readline()
+        st = st.replace("'", "\"")
+        ste = json.loads(st)
+        self.prob_name = ste["problems_name"]
+        self.description_tex = ste["legend"]
+        self.texToHtml(self.description_tex, "description")
+        self.inTex = ste["input"]
+        self.outTex = ste["output"]
+        text = "Input:\n\n" + self.inTex + "\n\nOutput:\n\n" + self.outTex
+        self.texToHtml(text, "io")
+
+        self.examplesTex = ""
+        for ioTex in ste["sampleTests"]:
+            examplesIn_tex = ioTex["input"].replace("\n", "\\newline")
+            examplesOut_tex = ioTex["output"].replace("\n", "\\newline")
+            self.examplesTex += "\n\ninput:\n\n" + examplesIn_tex + "\n\noutput:\n\n" + examplesOut_tex
+        self.texToHtml(self.examplesTex, "examples")
 
 
-        # self.up_text.setText(name)
-        self.up_text.setText("Потерянные улицы")
+        self.up_text.setText(self.prob_name)
         self.bot_frame = QToolBox()
-        # self.w = QWidget(self)
-        # self.pic = QPixmap("src/task01.png")
-        # self.label = QLabel(self.w)
-        # self.label.setPixmap(self.pic)
 
-        self.desc = "Компания РДЖ запланировала построить в стране новую железнодорожную сеть. В этой стране имеется n городов.\n\
-РДЖ запланировала построить m новых двунаправленных дорог, которые будут соединять города этой страны.\n\
-Разумеется, на строительство сети планируется потратить как можно меньше средств из бюджета, поэтому был \n\
-составлен план строительства, где был произведен расчет кратчайших расстояний между каждой парой городов. \n\
-План представляет собой матрицу, где на пересечении i-ой и j-ой строки записано число, равное кратчайшему \n\
-расстоянию между городами i и j. Железнодорожная сеть представляет собой набор участков железной дороги, \n\
-каждый участок соединяет два различных города. Между двумя любыми городами существует не более одного участка\n\
-железной дороги. Помогите восстановить дорожную сеть между городами, если возможно построить её согласно плану."
+        self.desc = QTextEdit()
+        fht = open("tmp/description.html")
+        txt = fht.read()
+        fht.close()
+        self.desc.setHtml(txt)
+        self.desc.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
 
+        self.io = QTextEdit()
+        fht = open("tmp/io.html")
+        txt = fht.read()
+        fht.close()
+        self.io.setHtml(txt)
+        self.io.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
 
-        self.io = 'Input:\n\
-В первой строке входных данных даны два целых числаnиm  количество городов\n\
-и дорог в стране(1<=n<=500,0<= m <=2000). В последующихnстроках содержится\n\
-таблица кратчайших расстояниймежду каждой парой городов, элементы таблицы - целые\n\
-неотрицательные числа, не превышающие10^6\n\
-Output:\nЕсли такая сеть существует, выведите в первой строке сообщение "YES" (без кавычек),\n\
-в противном случае   сообщение "NO" (без кавычек). сеть существует, то, \n\
-начиная со второй строки, выведитеmтроек целых чисел, описывающих участки\n\
-железной дороги. Каждый участок железной дороги описы-вается номерами городов,\n\
-которые им соединяются и его длину. Длина любого участка должна бытьположительнаи\n\
-не должна превышать10^6'
+        self.examp = QTextEdit()
+        fht = open("tmp/examples.html")
+        txt = fht.read()
+        fht.close()
+        self.examp.setHtml(txt)
+        self.examp.setTextInteractionFlags(QtCore.Qt.NoTextInteraction)
 
-
-        self.examp = 'input:\n\
-4 4\n\
-0 1 2 5\n\
-1 0 3 4\n\
-2 3 0 7\n\
-5 4 7 0\n\
-OUTPUT:\n\
-YES\n\
-2 1 1\n\
-1 3 2\n\
-2 4 4\n\
-1 4 5\n\
-INPUT:\n\
-3 2\n\
-0 1 1\n\
-1 0 1\n\
-1 1 0,\n\
-OUTPUT:\n\
-NO\n'
-
-        self.bot_frame.insertItem(0, QLabel(self.desc), "Desctiption")#"Вкладка &1")
-        # self.bot_frame.insertItem(0, self.w, "Desctiption")
-        # self.label.show()
-        # self.w.show()
-        self.bot_frame.insertItem(1, QLabel(self.io), "I/O Format")#"Вкладка &2")
-        self.bot_frame.insertItem(2, QLabel(self.examp), "Examples")
-        # self.bot_frame.insertItem(2, self.tableWidget, "Examples")
+        self.bot_frame.insertItem(0, self.desc, "Desctiption")
+        self.bot_frame.insertItem(1, self.io, "I/O Format")
+        self.bot_frame.insertItem(2, self.examp, "Examples")
 
         self.fst_frame.addWidget(self.up_frame)
         self.fst_frame.addWidget(self.bot_frame)
@@ -113,7 +96,7 @@ NO\n'
         self.snd_frame.setPalette(palette)
         self.val = 0.00
         self.snd_frame.setFormat('0.00')
-        self.totalMin = 5
+        self.totalMin = 3
 
         self.addWidget(self.fst_frame)
         self.addWidget(self.snd_frame)
@@ -127,30 +110,50 @@ NO\n'
         self.timer.start(1000)
 
 
-    # def update_task(self, task):
-    #     if task.type == TaskType.CODING:
-    #         pass
+    def update_task(self, task):
+        if task.type == TaskType.CODING:
+            pass
 
     def tick_status(self):
-        if self.window is None:
-            self.clear_back()
-            return
-        min = int(self.count // 60)
+        min = int(self.count/60)
         sec = int(self.count % 60)
         self.val = min + sec/100
         self.snd_frame.setValue(self.count*100/(self.totalMin*60))
         self.snd_frame.setFormat('%.02f' % self.val)
         self.count += 1
-        if self.count == 6:
+        if self.count == 3:
             self.window.chat_window.run_task(self.window.chat_tasks[1][1]) # yes no
-        if self.count == 12:
-            self.window.chat_window.run_task(self.window.chat_tasks[1][3])  # test
-        if self.count == 18:
-            self.window.chat_window.run_task(self.window.chat_tasks[1][5]) # single
-        if self.count > 300:
+        if self.count == 13:
+            self.window.chat_window.run_task(self.window.chat_tasks[1][5])  # test
+        if self.count == 23:
+            self.window.chat_window.run_task(self.window.chat_tasks[1][3]) # single
+        if self.count > 180:
             self.timer.stop()
 
-    def close(self):
-        self.timer.stop()
-        self.timer = None
-        super(WindowTask, self).close()
+
+    def textToTex(self, text):
+        prefTex = "\\documentclass{article}\n\
+\\usepackage[english,russian]{babel}\n\
+\\usepackage[T1]{fontenc}\n\
+\\usepackage{amsmath}\n\
+\\usepackage{amssymb}\n\
+\\usepackage[utf8]{inputenc}\n\
+\\begin{document}\n"
+        suffTex = "\n\\end{document}"
+        outTex = prefTex + text + suffTex
+        oTex = open("tempTex.tex", "w")
+        oTex.write(outTex)
+        oTex.flush()
+        oTex.close()
+        # time.sleep(2)
+
+    def texToHtml(self, text, name):
+        self.textToTex(text)
+        p = subprocess.Popen("htlatex tempTex.tex", shell=True)
+        p.wait()
+        p = subprocess.Popen("mv tempTex.html " + str(name) + ".html", shell=True)
+        p.wait()
+        p = subprocess.Popen("mv " + str(name) + ".html tmp", shell=True)
+        p.wait()
+        p = subprocess.Popen("rm tempTex.*", shell=True)
+        p.wait()
